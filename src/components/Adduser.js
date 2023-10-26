050b3d58fb4eaeeefe1026a193cbc150284ded9d
Dinesh Kumar <146819676+dineshkumar110784@users.noreply.github.com>
2023-10-26 14:32:30 +0530

    Create Adduser.js

---
+++ src/components/Adduser.js
@@ -0,0 +1,17 @@
+import React, { useEffect, useState } from 'react';
+import { Link } from "react-router-dom";
+import Paper from '@mui/material/Paper';
+
+
+function Adduser() {
+ 
+  return (
+    
+    <Paper sx={{ width: '100%'}}>
+      <h2 className='mx-3 mt-3'>Add New User</h2>
+
+   </Paper>
+  );
+}
+
+export default Adduser;
